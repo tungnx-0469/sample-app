772c5df2450933e8a41e3476dfe9cd4855f7a0e6
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2025-02-24 17:13:21 +0700

    Chapter 7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "msg.welcome_msg"
+      redirect_to @user, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = Settings.user_not_found_error
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation
+  end
+end
